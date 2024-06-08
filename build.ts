900d8ba587d39f9f042a676fcece1c16b6babce3
tialki <pxqing@outlook.com>
2024-06-08 15:30:44 +0800

    build: :building_construction:

--- build.ts
+++ build.ts
@@ -1,10 +1,8 @@
-/// <reference path="./src/bun-env.d.ts" />
-
 import dts from "bun-plugin-dts";
 
 await Bun.build({
   entrypoints: ["./src/client.ts"],
   outdir: "./dist",
   target: "bun",
-  plugins: [],
+  plugins: [dts()],
 });
