3789d6b93a3d2a2bdcd387d0f5e839bfc7cf8be1
tialki <pxqing@outlook.com>
2024-06-11 18:19:20 +0800

    build: :building_construction: Change tsconfig

--- build.ts
+++ build.ts
@@ -1,9 +1,9 @@
-import dts from "bun-plugin-dts";
+import dts from 'bun-plugin-dts'
 
 await Bun.build({
-  entrypoints: ["./src/index.ts"],
-  outdir: "./dist",
-  target: "bun",
+  entrypoints: ['./src/index.ts'],
+  outdir: './dist',
+  target: 'bun',
   plugins: [dts()],
-  external: ["@unservice/*", "@hono/*", "hono", "zod"],
-});
+  external: ['@unservice/*', '@hono/*', 'hono', 'zod'],
+})
